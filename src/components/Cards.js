c7a312a54c09bf12434b53b5381409576c856036
Ben Garcia <ben@seebenprogram.com>
2018-05-31 17:33:23 -0400

    render raw pokemon data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 // Stateless Functional Component syntax
 export const Cards = props => {
+  const pokemons = props.pokemons.map(pokemon => {
+    return (
+      <div
+        key={pokemon.name}
+      >
+        <img
+          src={pokemon.src}
+          alt={`Pokemon named ${pokemon.name}`}
+        />
+      </div>
+    );
+  })
   return (
     <main>
-      Cards
+      {pokemons}
     </main>
   );
 }
