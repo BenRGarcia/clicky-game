e7a23023c6e1753fa21b73e3629a51886241d9f8
Ben Garcia <ben@seebenprogram.com>
2018-05-31 17:04:40 -0400

    Add bold to paragraph

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const style = {
-  backgroundImage: `url(${require('../wormz.png')})`
+  backgroundImage: `url(${require('../wormz.png')})`,
+  border: '40px solid #000'
 };
 
 // Stateless Functional Component syntax
@@ -16,7 +17,7 @@ export const Header = props => {
           <h1 className="display-2 font-weight-bold">
             Clicky Game!
           </h1>
-          <p className="lead">
+          <p className="lead font-weight-bold">
             Click on an image to earn points, but don't click on any more than once!
           </p>
         </div>
