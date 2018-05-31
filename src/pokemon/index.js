327e6fe45c5b0cc3d34e8d70be13eb186695a7b9
Ben Garcia <ben@seebenprogram.com>
2018-05-31 17:14:37 -0400

    Add pokemon array

---
+++ src/pokemon/index.js
@@ -0,0 +1,50 @@
+export const pokemon = [
+  {
+    name: 'ditto',
+    src: `url(${require('./ditto.png')})`
+  },
+  {
+    name: 'hitmonlee',
+    src: `url(${require('./hitmonlee.png')})`
+  },
+  {
+    name: 'hypno',
+    src: `url(${require('./hypno.png')})`
+  },
+  {
+    name: 'kabuto',
+    src: `url(${require('./kabuto.png')})`
+  },
+  {
+    name: 'magmar',
+    src: `url(${require('./magmar.png')})`
+  },
+  {
+    name: 'mew',
+    src: `url(${require('./mew.png')})`
+  },
+  {
+    name: 'onix',
+    src: `url(${require('./onix.png')})`
+  },
+  {
+    name: 'pinsir',
+    src: `url(${require('./pinsir.png')})`
+  },
+  {
+    name: 'seel',
+    src: `url(${require('./seel.png')})`
+  },
+  {
+    name: 'shellder',
+    src: `url(${require('./shellder.png')})`
+  },
+  {
+    name: 'snorlax',
+    src: `url(${require('./snorlax.png')})`
+  },
+  {
+    name: 'tentacruel',
+    src: `url(${require('./tentacruel.png')})`
+  },
+];
