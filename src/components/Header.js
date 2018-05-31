0b68ae99af139ab7d5dec5576c8b33c303530fb0
Ben Garcia <ben@seebenprogram.com>
2018-05-31 16:01:53 -0400

    Add bg img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+const style = {
+  backgroundImage: 'url(' + require('../wormz.png') + ')'
+};
+
 // Stateless Functional Component syntax
 export const Header = props => {
   return (
     <header>
-      <div className="jumbotron jumbotron-fluid">
+      <div
+        className="jumbotron jumbotron-fluid"
+        style={style}
+      >
         <div className="container">
-          <h1 className="display-2 font-weight-bold">Clicky Game!</h1>
-          <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
+          <h1 className="display-2 font-weight-bold">
+            Clicky Game!
+          </h1>
+          <p className="lead">
+            Click on an image to earn points, but don't click on any more than once!
+          </p>
         </div>
       </div>
     </header>
