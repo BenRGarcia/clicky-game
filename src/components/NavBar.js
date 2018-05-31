05f632c6ca758ca5c63cd9ef902250e8b1e6aeb1
Ben Garcia <ben@seebenprogram.com>
2018-05-31 16:03:59 -0400

    Remove logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,13 +8,8 @@ const style = {
 // Stateless Functional Component syntax
 export const NavBar = props => {
   return (
-    <nav className='d-flex justify-content-between p-3' style={style.nav}>
-      <span>
-        Clicky Game!
-      </span>
-      <span>
-        <ScoresContainer />
-      </span>
+    <nav className='p-3' style={style.nav}>
+      <ScoresContainer />
     </nav>
   );
 }
