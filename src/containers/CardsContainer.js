5373999b7fb5c769a8dedb156dad2678b971cef9
Ben Garcia <ben@seebenprogram.com>
2018-05-31 17:13:37 -0400

    import pokemon

--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Cards } from '../components/Cards';
+import { pokemon } from '../pokemon/index';
 
 // Component Class syntax
 export class CardsContainer extends Component {
