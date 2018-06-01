fc18618216034f54653bf2e4305582cb15013547
Ben Garcia <ben@seebenprogram.com>
2018-05-31 22:20:26 -0400

    Build event handler scaffolding

--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -4,9 +4,34 @@ import { pokemons } from '../pokemon/index';
 
 // Component Class syntax
 export class CardsContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      charsClicked: [],
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(character) {
+    console.log(`${character} was clicked`)
+  }
+
+  addToCharsClicked() {
+
+  }
+
+  clearCharsClicked() {
+
+  }
+
   render() {
     return (
-      <Cards pokemons={pokemons} />
+      <Cards
+        pokemons={pokemons}
+        onClick={this.handleClick}
+      />
     );
   }
 }
