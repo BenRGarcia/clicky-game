81912476730f837e62860922089103dce73e4376
Ben Garcia <ben@seebenprogram.com>
2018-05-31 20:34:36 -0400

    Hard code initial state

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
+const style = {
+  border: '5px solid #000',
+  borderRadius: '8px',
+  boxShadow: '5px 10px 8px #888888'
+};
+
 // Stateless Functional Component syntax
 export const Cards = props => {
   const pokemons = props.pokemons.map(pokemon => {
     return (
       <div
         key={pokemon.name}
+        className='col-4 col-md-3 mb-3'
       >
         <img
           src={pokemon.src}
           alt={`Pokemon named ${pokemon.name}`}
+          className='img-fluid'
+          style={style}
         />
       </div>
     );
   })
   return (
-    <main>
+    <main className='row'>
       {pokemons}
     </main>
   );
