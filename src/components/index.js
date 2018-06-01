c203d0891cff0fe586243176f0ddab418c702062
Ben Garcia <ben@seebenprogram.com>
2018-06-01 19:40:09 -0400

    add index file for bulk exports

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import { Cards } from './Cards';
+import { Footer } from './Footer';
+import { Header } from './Header';
+import { NavBar } from './NavBar';
+import { Score } from './Score';
+
+export { 
+  Cards, 
+  Footer, 
+  Header,
+  NavBar, 
+  Score
+};
