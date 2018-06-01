6a0716e3dc9299641fc87b5103e1c4849745856d
Ben Garcia <ben@seebenprogram.com>
2018-06-01 18:57:46 -0400

    Refactor for brevity

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,22 @@ const style = {
   border: '40px solid #000'
 };
 
-export const Header = props => {
-  return (
-    <header className='mb-3'>
-      <div
-        className="jumbotron jumbotron-fluid bg-white mb-0"
-        style={style}>
-        <div className="container">
-          <h1 className="display-4 font-weight-bold">
-            Gotta click&nbsp;'em all!
-          </h1>
-          <p className="lead font-weight-bold">
-            (Just don't click the same one twice)
-          </p>
-        </div>
+const title = `Gotta click&nbsp;'em all!`;
+const subtitle = `(Just don't click the same one twice)`;
+
+export const Header = () => (
+  <header className='mb-3'>
+    <div
+      className="jumbotron jumbotron-fluid bg-white mb-0"
+      style={style}>
+      <div className="container">
+        <h1 className="display-4 font-weight-bold">
+          {title}
+        </h1>
+        <p className="lead font-weight-bold">
+          {subtitle}
+        </p>
       </div>
-    </header>
-  );
-}
+    </div>
+  </header>
+);
