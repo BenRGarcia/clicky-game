3bca0d0b288fbdf49c74680d51f710047f8b52aa
Ben Garcia <ben@seebenprogram.com>
2018-05-31 20:16:34 -0400

    correct img path

--- src/pokemon/index.js
+++ src/pokemon/index.js
@@ -1,50 +1,50 @@
 export const pokemons = [
   {
     name: 'ditto',
-    src: `url(${require('./ditto.png')})`
+    src: require('./ditto.png')
   },
   {
     name: 'hitmonlee',
-    src: `url(${require('./hitmonlee.png')})`
+    src: require('./hitmonlee.png')
   },
   {
     name: 'hypno',
-    src: `url(${require('./hypno.png')})`
+    src: require('./hypno.png')
   },
   {
     name: 'kabuto',
-    src: `url(${require('./kabuto.png')})`
+    src: require('./kabuto.png')
   },
   {
     name: 'magmar',
-    src: `url(${require('./magmar.png')})`
+    src: require('./magmar.png')
   },
   {
     name: 'mew',
-    src: `url(${require('./mew.png')})`
+    src: require('./mew.png')
   },
   {
     name: 'onix',
-    src: `url(${require('./onix.png')})`
+    src: require('./onix.png')
   },
   {
     name: 'pinsir',
-    src: `url(${require('./pinsir.png')})`
+    src: require('./pinsir.png')
   },
   {
     name: 'seel',
-    src: `url(${require('./seel.png')})`
+    src: require('./seel.png')
   },
   {
     name: 'shellder',
-    src: `url(${require('./shellder.png')})`
+    src: require('./shellder.png')
   },
   {
     name: 'snorlax',
-    src: `url(${require('./snorlax.png')})`
+    src: require('./snorlax.png')
   },
   {
     name: 'tentacruel',
-    src: `url(${require('./tentacruel.png')})`
-  },
+    src: require('./tentacruel.png')
+  }
 ];
