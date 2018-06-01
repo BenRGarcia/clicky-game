c4752fbb5fb759580d2a1537f9f3177b8b485c3b
Ben Garcia <ben@seebenprogram.com>
2018-06-01 18:55:03 -0400

    Refactor for brevity

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-export const Footer = props => {
-  return (
-    <footer className='bg-danger p-2 fixed-bottom'>
-      <a
-        href='https://github.com/BenRGarcia/clicky-game'
-        target='_blank'
-        className='text-white'
-        rel='noopener noreferrer'
-      >
-        View Source Code
-      </a>
-    </footer>
-  );
-}
+export const Footer = () => (
+  <footer className='bg-danger p-2 fixed-bottom'>
+    <a
+      href='https://github.com/BenRGarcia/clicky-game'
+      target='_blank'
+      className='text-white'
+      rel='noopener noreferrer'>
+      View Source Code
+    </a>
+  </footer>
+);
