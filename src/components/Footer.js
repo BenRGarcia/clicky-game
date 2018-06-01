3246f5a12ae63d50b1052b2100146f8f9f9135b1
Ben Garcia <ben@seebenprogram.com>
2018-05-31 20:39:20 -0400

    chnage bg color to red

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Stateless Functional Component syntax
 export const Footer = props => {
   return (
-    <footer className='bg-secondary p-2 fixed-bottom'>
+    <footer className='bg-danger p-2 fixed-bottom'>
       <a
         href='https://github.com/BenRGarcia/clicky-game'
         target='_blank'
