562426d680aaa330759759ebd7b50a7e0c291f3d
Ben Garcia <ben@seebenprogram.com>
2018-05-31 22:21:39 -0400

    build scaffolding of class methods

--- src/containers/ScoresContainer.js
+++ src/containers/ScoresContainer.js
@@ -4,12 +4,36 @@ import { TopScore } from '../components/TopScore';
 
 // Component Class syntax
 export class ScoresContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      score: 0,
+      topScore: 0
+    };
+
+    this.updateScore = this.updateScore.bind(this);
+    this.updateTopScore = this.updateTopScore.bind(this);
+  }
+
+  updateScore(newScore) {
+    this.setState({
+      score: newScore
+    });
+  }
+
+  updateTopScore(newScore) {
+    this.setState({
+      topScore: newScore
+    });
+  }
+
   render() {
     return (
       <span>
         <Score />
         &nbsp;&nbsp;|&nbsp;&nbsp;
-        <TopScore />
+        <TopScore  />
       </span>
     );
   }
